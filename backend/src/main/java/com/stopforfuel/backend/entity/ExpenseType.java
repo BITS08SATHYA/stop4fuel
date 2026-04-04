551271f1b4f52d1a8229606a30ccb611dab094e7
Sathya <neelasathya06@gmail.com>
2026-04-04 00:53:54 -0400

    Fix expense type dropdown: add @JsonProperty name to match frontend expectation

--- backend/src/main/java/com/stopforfuel/backend/entity/ExpenseType.java
+++ backend/src/main/java/com/stopforfuel/backend/entity/ExpenseType.java
@@ -1,4 +1,5 @@
 package com.stopforfuel.backend.entity;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,5 +10,6 @@ import lombok.Setter;
 public class ExpenseType extends SimpleBaseEntity {
     @jakarta.validation.constraints.NotBlank(message = "Expense type name is required")
     @Column(name = "type_name", nullable = false)
+    @JsonProperty("name")
     private String typeName;
 }
