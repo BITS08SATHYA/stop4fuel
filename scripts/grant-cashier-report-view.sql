29e8f21f2b04f1bb8033f0f9e18cf9b708baf62b
Sathya <neelasathya06@gmail.com>
2026-04-14 18:40:10 -0400

    scripts: SQL to grant REPORT_VIEW to CASHIER

---
+++ scripts/grant-cashier-report-view.sql
@@ -0,0 +1,16 @@
+-- Grant REPORT_VIEW permission to CASHIER role (idempotent).
+INSERT INTO role_permission (role_id, permission_id)
+SELECT r.id, p.id
+FROM roles r, permission p
+WHERE r.role_type = 'CASHIER'
+  AND p.code = 'REPORT_VIEW'
+  AND NOT EXISTS (
+    SELECT 1 FROM role_permission rp
+    WHERE rp.role_id = r.id AND rp.permission_id = p.id
+  );
+
+SELECT r.role_type, p.code
+FROM role_permission rp
+JOIN roles r ON r.id = rp.role_id
+JOIN permission p ON p.id = rp.permission_id
+WHERE r.role_type = 'CASHIER' AND p.code = 'REPORT_VIEW';
